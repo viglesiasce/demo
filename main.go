9455f6287c5bfca318ea8ab66345d891aa9b3479
Vic Iglesias <viglesias@google.com>
2018-06-20 13:16:50 -0700

    Add resource limits

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"time"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Host: %s\n", os.Getenv("HOSTNAME"))
+	fmt.Printf("Received request from: %s\n", r.Header.Get("X-Forwarded-For"))
+}
+
 func main() {
-	for {
-		fmt.Printf("Hello world from %s!\n", os.Getenv("HOSTNAME"))
-		time.Sleep(time.Second * 1)
-	}
+	go func() {
+		for {
+			fmt.Printf("CPU Limit: %s\n", os.Getenv("MY_CPU_LIMIT"))
+			time.Sleep(time.Second * 5)
+		}
+	}()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
